085feb1bd563a6f9bfcdab39c2f92da816ed3fce
connortroyrogers <connortroyrogers@gmail.com>
2024-03-11 15:50:44 -0400

    update config

--- init.lua
+++ init.lua
@@ -6,20 +6,31 @@ vim.call('plug#begin')
 --vim.call("plug#begin(has('nvim') ? stdpath('data') . '/plugged' : '~/.vim/plugged')")
 
 --Declare the list of plugins.
+--Autoclose
 Plug ('m4xshen/autoclose.nvim')
+
 Plug ('cohama/lexima.vim')
+
+--Plenary
 Plug ('nvim-lua/plenary.nvim')
+
+--Harpoon
 Plug ('https://github.com/ThePrimeagen/harpoon', {['branch'] = 'harpoon2'})
+
 --Telescope
 Plug ('nvim-lua/plenary.nvim')
 Plug ('nvim-telescope/telescope.nvim', {['tag']= '0.1.5'})
+
 --UndoTree
 Plug ('mbbill/undotree')
+
 --Treesitter
 Plug ('nvim-treesitter/nvim-treesitter', {['do']= ':TSUpdate'})
 Plug ('nvim-treesitter/playground')
+
 --LSP Support
 Plug ('neovim/nvim-lspconfig')
+
 --Autocompletion
 Plug ('hrsh7th/nvim-cmp')
 Plug ('hrsh7th/cmp-nvim-lsp')
@@ -33,9 +44,16 @@ Plug ('neovim/nvim-lspconfig')
 --Fugitive (Git status)
 Plug ('tpope/vim-fugitive')
 
+--Rose Pine
+Plug ('rose-pine/neovim')
+
+--Vim Be Good
+Plug ('ThePrimeagen/vim-be-good')
+
 --List ends here. Plugins become visible to Vim after this call.
 vim.call('plug#end')
 
 vim.opt.shiftwidth=4
 vim.opt.tabstop=4
+vim.cmd("colorscheme rose-pine")
 
